989aa339f608b4b4296acef6686a49aa50491876
Achmad Daniel Syahputra <achmad24daniel@gmail.com>
2025-08-15 00:25:32 +0700

    create HomeActivity layout

--- app/src/main/java/com/nielcode/kupass/ui/activities/HomeActivity.java
+++ app/src/main/java/com/nielcode/kupass/ui/activities/HomeActivity.java
@@ -2,15 +2,21 @@ package com.nielcode.kupass.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.nielcode.kupass.R;
 import com.nielcode.kupass.core.Permission;
 import com.nielcode.kupass.databinding.ActivityHomeBinding;
+import com.nielcode.kupass.ui.adapters.ListPasswordAdapter;
+import com.nielcode.kupass.ui.adapters.ListPasswordItem;
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
+  private ArrayList<ListPasswordItem> listPasswordItems;
+  private ListPasswordAdapter adapter;
   private ActivityHomeBinding binding;
   private Permission permissionHandler;
 
@@ -23,6 +29,15 @@ public class HomeActivity extends AppCompatActivity {
     // Check permission
     checkPermission();
     setupButtonListener();
+
+    listPasswordItems = new ArrayList<>();
+
+    // Add dummy data
+    for (int i = 0; i < 20; i++) {
+      listPasswordItems.add(new ListPasswordItem(i, "Password", "Username", "Password", "Note"));
+    }
+
+    update();
   }
 
   @Override
@@ -64,5 +79,37 @@ public class HomeActivity extends AppCompatActivity {
           .create()
           .show();
     }
+  }
+
+  private void update() {
+    // listPasswordItems = sql.getAll();
+    adapter = new ListPasswordAdapter(this, listPasswordItems);
+
+    binding.listPassword.setAdapter(adapter);
+    binding.listPassword.setOnItemClickListener(
+        (adapter, view, position, id) -> {
+          // Intent intent = new Intent(this, PasswordPage.class);
+          // intent.putExtra("list", (Serializable) mListItem.get(position));
+          // startActivity(intent);
+        });
+    binding.listPassword.setOnItemLongClickListener(
+        (adapter, view, position, id) -> {
+          new MaterialAlertDialogBuilder(this)
+              .setTitle(this.getString(R.string.dialog_title_delete))
+              .setMessage(this.getString(R.string.dialog_message_delete))
+              .setNegativeButton(this.getString(R.string.dialog_cancel), (dialog, which) -> {})
+              .setPositiveButton(
+                  this.getString(R.string.dialog_delete),
+                  (dialog, which) -> {
+                    // mSQL.delete(listPasswordItems.get(position).getId());
+                    Toast.makeText(
+                            this, this.getString(R.string.toast_success_delete), Toast.LENGTH_SHORT)
+                        .show();
+                    update();
+                  })
+              .create()
+              .show();
+          return true;
+        });
   }
 }
