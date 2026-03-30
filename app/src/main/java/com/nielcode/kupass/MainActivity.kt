2db70e86d3e1a2f109a749226742444ecea8e506
Achmad Daniel Syahputra <achmad24daniel@gmail.com>
2026-03-30 08:35:08 +0700

    refactor: remove unused compose preview functions from MainActivity
    The Greeting composable and GreetingPreview preview function were not used in the app, only serving as default template code. Removing them reduces clutter and improves code maintainability.
    
    Signed-off-by: Achmad Daniel Syahputra <achmad24daniel@gmail.com>

--- app/src/main/java/com/nielcode/kupass/MainActivity.kt
+++ app/src/main/java/com/nielcode/kupass/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.nielcode.kupass.ui.screens.home.HomeScreen
 import com.nielcode.kupass.ui.theme.KupassTheme
 
@@ -17,15 +13,4 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent { KupassTheme { HomeScreen() } }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!", modifier = modifier)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KupassTheme { Greeting("Android") }
 }
