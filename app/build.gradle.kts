6aaef7ced3c01b56258a8de65907dbb1ad9b848b
Achmad Daniel Syahputra <achmad24daniel@gmail.com>
2026-04-08 11:22:02 +0700

    build: downgrade Java compatibility from 17 to 11
    Change sourceCompatibility and targetCompatibility to JavaVersion.VERSION_11 to ensure compatibility with older JDK versions or build environments that do not yet support Java 17.
    
    Signed-off-by: Achmad Daniel Syahputra <achmad24daniel@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,8 +81,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     buildFeatures {
